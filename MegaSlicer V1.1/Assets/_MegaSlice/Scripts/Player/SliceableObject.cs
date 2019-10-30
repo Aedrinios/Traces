058f21da0a8085eeb1415c10e3fa6c4388d7511a
Aedrinios <clementclerc2@gmail.com>
2019-10-30 14:17:02 +0100

    Slice first proto LD

---
+++ MegaSlicer V1.1/Assets/_MegaSlice/Scripts/Player/SliceableObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(MeshCollider))]
+public class SliceableObject : MonoBehaviour
+{
+    GameObject player; 
+
+    void Start()
+    {
+        gameObject.layer = LayerMask.NameToLayer("Sliceable");        
+        GetComponent<MeshCollider>().convex = true;
+
+        player = GameObject.FindWithTag("Player");
+        Vector3 expulsion = transform.position - player.transform.position;
+        float push = player.GetComponent<PlayerAttack>().forcePush;
+        GetComponent<Rigidbody>().AddForce(expulsion * push);
+ 
+    }
+}
