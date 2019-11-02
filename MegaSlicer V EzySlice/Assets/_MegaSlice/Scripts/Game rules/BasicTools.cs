25a1322f3f9c26d1f283b56fa95edd41d3e8f965
Rosam-dev <blot.paulgd@gmail.com>
2019-11-02 15:32:39 +0100

    Dis moi Jamie
    c'est très simple !!!

---
+++ MegaSlicer V EzySlice/Assets/_MegaSlice/Scripts/Game rules/BasicTools.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class BasicTools : MonoBehaviour
+{
+    public static void NextScene()
+    {
+        int lengthScenes = SceneManager.sceneCountInBuildSettings;
+        int numberNextScene = SceneManager.GetActiveScene().buildIndex + 1;
+        
+        if (numberNextScene < lengthScenes)
+        {
+            SceneManager.LoadScene(numberNextScene);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    public static void RestartScene()
+    {
+    	if (Input.GetKeyDown(KeyCode.R))
+        {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
+    	}
+    }
+
+    public static void QuitGame()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    private void Update()
+    {
+        RestartScene();
+        QuitGame(); 
+    }
+}
