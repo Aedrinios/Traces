0b6fe1f1b112d7848b77bfd0eb9b60fb0ff8bef3
Aedrinios <clementclerc2@gmail.com>
2019-11-23 18:53:59 +0100

    little modif to enemy behaviour (now faster with detection zone) and armor enemy (added armor count)

--- MegaSlicer V EzySlice/Assets/_MegaSlice/Scripts/AI/EnemyBehaviour.cs
+++ MegaSlicer V EzySlice/Assets/_MegaSlice/Scripts/AI/EnemyBehaviour.cs
@@ -8,7 +8,8 @@ public class EnemyBehaviour : MonoBehaviour
     private GameObject target;
     private NavMeshAgent agent;
 
-    [SerializeField] private float speed;
+    [SerializeField] private float agentSpeed;
+    [SerializeField] private float detectionDistance;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +21,20 @@ public class EnemyBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.transform.position);
+        float playerDistance = Vector3.Distance(GameObject.Find("Player").transform.position, transform.position);
+        if(playerDistance < detectionDistance)
+        {
+            gameObject.GetComponent<NavMeshAgent>().isStopped = false;
+
+            agent.speed = agentSpeed;
+            agent.SetDestination(target.transform.position);
+        }
+        else
+        {
+            gameObject.GetComponent<NavMeshAgent>().isStopped = true;
+        }
+
+
     }
 
     private void OnCollisionEnter(Collision collision)
