38fec90c2b0890a2600abbbc8e013d48876d2678
Dryuko <57187036+Dryuko@users.noreply.github.com>
2019-11-27 16:13:10 +0100

    Début test urban scene

--- MegaSlicer V EzySlice/Assets/AssetStoreOriginals/_SNAPS_PrototypingAssets/Scripts/Editor/PackageChecker/PackageChecker.cs
+++ MegaSlicer V EzySlice/Assets/AssetStoreOriginals/_SNAPS_PrototypingAssets/Scripts/Editor/PackageChecker/PackageChecker.cs
@@ -37,7 +37,7 @@ namespace SNAP
                 var packageListFile = Directory.GetFiles(Application.dataPath, "PackageImportList.txt", SearchOption.AllDirectories);
                 if (packageListFile.Length == 0)
                 {
-                    //Debug.LogError("[Auto Package] : Couldn't find the packages list. Be sure there is a file called PackageImportList in your project");
+                    Debug.LogError("[Auto Package] : Couldn't find the packages list. Be sure there is a file called PackageImportList in your project");
                 }
                 else
                 {
