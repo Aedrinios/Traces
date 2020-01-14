3a52a6622f0c8c54e5e2053440fad9e50479ce96
Blot Paul <blot.paulluap@sfr.fr>
2020-01-14 19:00:10 +0100

    Curseur

--- Slice And Run v2/Assets/_MegaSlice/Scripts/Player/RotateArrow.cs
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/Player/RotateArrow.cs
@@ -5,11 +5,12 @@ using UnityEngine.UI;
 
 public class RotateArrow : MonoBehaviour
 {
+    public Vector3 decalAngle = Vector3.zero; 
     private float rotationZ;
 
     private void Update()
     {
         rotationZ = MouseControl.angle;
-        transform.localEulerAngles = new Vector3(0, 0, rotationZ);
+        transform.localEulerAngles = new Vector3(0, 0, rotationZ) + decalAngle;
     }
 }
