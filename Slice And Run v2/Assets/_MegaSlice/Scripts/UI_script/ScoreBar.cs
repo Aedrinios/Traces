e2daa12b7633e3e36ba013306d06be1e01f3d87a
Blot Paul <blot.paulluap@sfr.fr>
2020-01-14 22:07:43 +0100

    L'UI est beau

--- Slice And Run v2/Assets/_MegaSlice/Scripts/UI_script/ScoreBar.cs
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/UI_script/ScoreBar.cs
@@ -5,10 +5,6 @@ using UnityEngine.UI;
 
 public class ScoreBar : MonoBehaviour
 {
-	
-	 Color colorWarning = Color.yellow;
-	public Color colorDanger = Color.red;
-
 	LifeTimerManager fifeTimerManager; 
 	Image img;
 	float ratioFill;
@@ -23,25 +19,7 @@ public class ScoreBar : MonoBehaviour
 
 	private void Update()
 	{
-
 		ratioFill = LifeTimerManager.lifeTimer / fifeTimerManager.timerMax;
-
 		img.fillAmount = ratioFill;
-
-		if (ratioFill <= 0.1f)
-		{
-			img.color = colorDanger;
-		}
-		else if (ratioFill <= 0.33f)
-		{
-			img.color = colorWarning;
-		}
-		else
-		{
-			img.color = originalColor;
-		}
-
-
-
 	}
 }
