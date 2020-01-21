f46040ca8d1bf16a55f93bc62a416e3884317482
Blot Paul <blot.paulluap@sfr.fr>
2020-01-21 15:49:28 +0100

    Detect Landing

---
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/FmodParameter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class FmodParameter : MonoBehaviour
+{
+    FMOD.Studio.EventInstance instance;
+    [FMODUnity.EventRef]
+    public string fmodEvent;
+    public string nameParameter;
+    [Range(0, 1f)] public float valueParameter; 
+
+    private void OnEnable()
+    {
+        instance = RuntimeManager.CreateInstance(fmodEvent);
+        instance.start(); 
+    }
+
+    private void Update()
+    {
+        if (nameParameter != null)
+        {
+            instance.setParameterByName(nameParameter, valueParameter);
+        }
+    }
+
+    private void OnDisable()
+    {
+        instance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE); 
+    }
+}
---
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/Player/DetectLanding.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events; 
+
+public class DetectLanding : MonoBehaviour
+{
+    public UnityEvent LandingEvent; 
+    CharacterController characterController;
+    float chrono = 0; 
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>(); 
+    }
+
+    private void Update()
+    {
+        if (!characterController.isGrounded)
+        {
+            chrono += Time.deltaTime;
+        }
+        else
+        {
+            if (chrono >= 0.5f) LandingEvent.Invoke(); 
+            chrono = 0; 
+        }
+    }
+}
