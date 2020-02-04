2eadc5c9498a83b15959ea4f133a268953c76fcc
Aedrinios <clementclerc2@gmail.com>
2020-02-04 23:09:20 +0100

    scene transition

--- Slice And Run v2/Assets/_MegaSlice/Scripts/Game rules/GameManager.cs
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/Game rules/GameManager.cs
@@ -24,15 +24,69 @@ public class GameManager : MonoBehaviour
 
     #endregion
 
+    private GameObject loadingScreen;
+    private Image loadingBar;
+
     public Transform playerPosition;
 
     public float forcePushCut = 80;
 	public static float forcePushCutStc;
 
-	private void Awake()
-	{
-        DontDestroyOnLoad(this.gameObject);
-		forcePushCutStc = forcePushCut;
-        playerPosition = GameObject.Find("StartSpawner").transform;
+    private void Awake()
+    {
+      //  DontDestroyOnLoad(this.gameObject);
+        forcePushCutStc = forcePushCut;
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    public void LaunchLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadScene(sceneIndex));
+    }
+
+    private IEnumerator LoadScene(int id)
+    {
+        loadingScreen.SetActive(true);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(id);
+        loadingBar.fillAmount = 0f;
+
+        while (!asyncLoad.isDone)
+        {
+            loadingBar.fillAmount = asyncLoad.progress;
+            yield return null;
+        }
+
+        loadingScreen.SetActive(false);
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        loadingScreen = GameObject.Find("Canvas").transform.Find("LoadingScreen").gameObject;
+        loadingBar = loadingScreen.transform.GetChild(2).gameObject.GetComponent<Image>();
+        if (scene.buildIndex == 0)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
--- Slice And Run v2/Assets/_MegaSlice/Scripts/Game rules/HeartScript.cs
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/Game rules/HeartScript.cs
@@ -1,11 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class HeartScript : MonoBehaviour
 {
     [Header("Time control")]
-    [SerializeField] private float timeSlowDown;
+    [SerializeField] private float slowPower;
     [SerializeField] private float smooth;
     [SerializeField] private float slowDuration;
 
@@ -22,14 +22,22 @@ public class HeartScript : MonoBehaviour
 
     private void Update()
     {
+        float showTimeScale = Time.timeScale;
+        Debug.Log(showTimeScale);
         if (slowed)
         {
-            Time.timeScale = Mathf.Lerp(Time.timeScale, timeSlowDown, Time.unscaledDeltaTime * smooth);
+            Time.timeScale = Mathf.Lerp(Time.timeScale, slowPower, Time.unscaledDeltaTime * smooth);
             timePast += Time.unscaledDeltaTime;
             if (timePast > slowDuration)
             {
                 slowed = false;
                 timePast = 0f;
+                Time.timeScale = originalTimeScale;
+
+                if (SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCountInBuildSettings - 1)
+                    GameManager.Instance.LaunchLevel(SceneManager.GetActiveScene().buildIndex + 1);
+                else
+                    GameManager.Instance.LaunchLevel(0);
             }
         }
         else
