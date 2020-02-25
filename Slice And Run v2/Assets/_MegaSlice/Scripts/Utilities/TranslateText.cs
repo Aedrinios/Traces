a5304b961be9f0575228e5c0c569a9122904a5dd
Blot Paul <blot.paulluap@sfr.fr>
2020-02-25 17:51:43 +0100

    Traduction en français

---
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/Utilities/TranslateText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+
+public class TranslateText : MonoBehaviour
+{
+    [TextArea] public string frenchText;
+    public float modifSize = 0f; 
+
+    private void Start()
+    {
+        TranslateTextPro(); 
+    }
+
+    void TranslateTextPro()
+    {
+        if (GetComponent<TextMeshProUGUI>())
+        {
+            TextMeshProUGUI textMesh = GetComponent<TextMeshProUGUI>();
+            textMesh.text = frenchText;
+            textMesh.fontSize += modifSize; 
+        }
+        if (GetComponent<TextMeshPro>())
+        {
+            TextMeshPro textMesh = GetComponent<TextMeshPro>();
+            textMesh.text = frenchText;
+            textMesh.fontSize += modifSize;
+        }
+
+    }
+}
