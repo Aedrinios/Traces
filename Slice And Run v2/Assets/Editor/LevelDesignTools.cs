f99aa08b6c5957cd96c9ee51cd0df459c1485a7b
Blot Paul <blot.paulluap@sfr.fr>
2020-03-05 16:13:41 +0100

    WIP outil PlaceOnCicle

--- Slice And Run v2/Assets/Editor/LevelDesignTools.cs
+++ Slice And Run v2/Assets/Editor/LevelDesignTools.cs
@@ -12,20 +12,21 @@ public class LevelDesignTools : EditorWindow
 		GetWindow<LevelDesignTools>("LevelDesignTools");
 	}
 
-
+	SaveScriptableObject data;
 	public GameObject[] essentialsGameObjects;
-
-	SaveScriptableObject data; 
+	public float radiusCircle = 1; 
 	
 	//rajout des fonctionalités
 	private void OnGUI()
 	{
 		GUILayout.Label("Fenetre pour aider le Level Design", EditorStyles.boldLabel);
 
+		// list contenant les gameobjects essentiels
 		SerializedObject so = new SerializedObject(this);
 		EditorGUILayout.PropertyField(so.FindProperty("essentialsGameObjects"), true);
 		so.ApplyModifiedProperties();
 
+		// bouton pour placer les objets essentiels
 		if (GUILayout.Button("Mettre les objects essentiels"))
 		{
 			GameObject newParent = new GameObject("Essentials"); 
@@ -37,8 +38,46 @@ public class LevelDesignTools : EditorWindow
 				newObject.transform.parent = newParent.transform; 
 			}
 		}
+
+		GUILayout.Label("\n Les paramètres pour placer les objets en rond", EditorStyles.boldLabel);
+
+		radiusCircle = EditorGUILayout.FloatField("Le rayon du cercle est : ", radiusCircle); 
+		if (GUILayout.Button("Placer les objets sur un cercle"))
+		{
+			PlaceOnCircle(radiusCircle); 
+		}
+
 	}
 
+	void PlaceOnCircle(float radius)
+	{
+		GameObject[] selectionObject = Selection.gameObjects;
+		if (selectionObject.Length > 1)
+		{
+			for (int i = 0; i < selectionObject.Length; i++)
+			{
+				float angle = 6.283f / (selectionObject.Length);
+				float angle_x = Mathf.Cos(i * angle);
+				float angle_y = Mathf.Sin(i * angle);
+
+				Vector3 dir = new Vector3(angle_x, angle_y, 0);
+				selectionObject[i].transform.localPosition = Vector3.zero;
+				selectionObject[i].transform.localPosition = dir * radius;
+
+				float tourne = 360 / (selectionObject.Length) * i;
+				selectionObject[i].transform.rotation = Quaternion.Euler(new Vector3(0, 0, tourne + 90));
+			}
+		}
+		else
+		{
+			Debug.Log("Pas assez d'objets sélectionnés"); 
+		}
+
+	}
+
+
+
+	#region
 	private void OnEnable()
 	{
 		data = Resources.Load<SaveScriptableObject>("LevelDataData");
@@ -50,6 +89,6 @@ public class LevelDesignTools : EditorWindow
 		data = Resources.Load<SaveScriptableObject>("LevelDataData");
 		data.saveObjects = essentialsGameObjects;
 	}
-
+	#endregion
 
 }
