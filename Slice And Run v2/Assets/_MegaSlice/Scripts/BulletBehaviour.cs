7d59ccc3900438f158fcf8920481bfd481052f41
Aedrinios <clementclerc2@gmail.com>
2020-03-10 17:07:57 +0100

    Canon WIP

---
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/BulletBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    [SerializeField] private float bulletSpeed;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(bulletSpeed * transform.forward);
+    }
+}
---
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/CanonBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CanonBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+    private Transform muzzle;
+    private Transform turret;
+    private Transform target;
+    private bool isReloading;
+    [SerializeField] private float reloadTimer;
+    [SerializeField] private bool followPlayer;
+    private float timer = 0f;
+
+    private void Start()
+    {
+        turret = transform.Find("Turret");
+        muzzle = turret.Find("Muzzle");
+
+        target = GameObject.Find("Player").transform;
+    }
+
+    void Update()
+    {
+        if(followPlayer)
+            turret.LookAt(target);
+
+        if (isReloading)
+        {
+            timer += Time.deltaTime;
+            if (timer >= reloadTimer)
+            {
+                timer = 0;
+                isReloading = false;
+            }
+        }
+        else
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, muzzle.position, Quaternion.LookRotation(turret.forward));
+        isReloading = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collision");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("is this a collision ?");
+        }
+    }
+}
