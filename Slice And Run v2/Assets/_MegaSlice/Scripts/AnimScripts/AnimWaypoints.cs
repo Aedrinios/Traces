a82556f2e2671b07ecc1e6e75ec570e31a869257
Blot Paul <blot.paulluap@sfr.fr>
2020-03-20 16:12:34 +0100

    Ajout de l'AnimWaypoints et des plateformes

---
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/AnimScripts/AnimWaypoints.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimWaypoints : MonoBehaviour
+{
+    public Transform moveBody;
+    public Transform waypoints;
+
+    public float speed = 8;
+    int way = 0; 
+
+    List<Transform> allWaypoints = new List<Transform>(); 
+
+    private void Start()
+    {
+        int childLenght = waypoints.childCount; 
+        for (int i = 0; i < childLenght; i++)
+        {            
+            allWaypoints.Add(waypoints.GetChild(i)); 
+        }
+        way = 1; 
+    }
+
+    private void FixedUpdate()
+    {
+        modifWay();
+        float absSpeed = Mathf.Abs(speed); 
+        moveBody.position = Vector3.MoveTowards(moveBody.position, allWaypoints[way].position, absSpeed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        checkIfExist();
+    }
+
+    void modifWay()
+    {
+        if (Vector3.Distance(moveBody.position, allWaypoints[way].position) <= 0.1f)
+        {
+            if (speed >= 0)
+            {
+                way++;
+                if (way >= allWaypoints.Count)
+                {
+                    way = 0;
+                }
+            }
+            else if (speed < 0)
+            {
+                way--;
+                if (way < 0)
+                {
+                    way = allWaypoints.Count -1;
+                }
+            }
+
+        }
+    }
+
+    void checkIfExist()
+    {
+        if (moveBody == null)
+        {
+            Debug.Log("MoveObjet deleted !!!");
+            Destroy(this); 
+        }
+    }
+}
---
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/LevelDesignScripts/PlatformSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSystem : MonoBehaviour
+{
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			other.gameObject.transform.parent = transform;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			other.gameObject.transform.parent = null;
+		}
+	}
+
+}
