32190fc0a8f576ad943a47f217579ba7fa579140
Blot Paul <blot.paulluap@sfr.fr>
2020-03-20 14:45:14 +0100

    Ventilo opérationnel

--- Slice And Run v2/Assets/_MegaSlice/Scripts/LevelDesignScripts/BlowerSystem.cs
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/LevelDesignScripts/BlowerSystem.cs
@@ -5,53 +5,40 @@ using UnityEngine;
 public class BlowerSystem : MonoBehaviour
 {
     public GameObject zone;
-    public float powerMax = 20;
+    public float powerMax = 120f;
+    public float ratio = 0.3f;
 
-    public AnimationCurve curve;  
+    //public AnimationCurve curve;  
 
     FPS_Controller fps;
 
-    float size;
+    //float size;
     float power; 
 
     private void Start()
     {
         fps = GameObject.FindWithTag("Player").GetComponent<FPS_Controller>();
-        //originalPower = power; 
-
-        Vector3 colMesh = zone.GetComponent<MeshFilter>().mesh.bounds.size;
-        Vector3 scale = zone.transform.localScale; 
-        size = colMesh.y * scale.y;
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            PowerSmooth(); 
-            //EquilibreForce(); 
+            EquilibreForce(); 
 
             fps.PushPlayer(Vector3.up * power * Time.deltaTime);
         }
     }
 
-    // l'idée serait de donner une force au joueur en fonction de sa vitesse de chute pour le stabiliser
     void EquilibreForce()
     {
-        power = fps.gravity; 
-    }
-
-
-    // j'arrive pas à faire marcher la curve
-    void PowerSmooth()
-    {
-        float distance =  transform.position.y - FPS_Controller.playerPos.y;
-        distance = Mathf.Abs(distance);
-
-        float ratioDistance = distance / size;
-        ratioDistance = 1- ratioDistance;  
-        ratioDistance = Mathf.Clamp(ratioDistance, 0f, 1f);
-        
-        power = powerMax * curve.Evaluate(ratioDistance); 
+        if (fps.velocity.y < 0)
+        {
+            power = powerMax;
+        }
+        else
+        {
+            power = powerMax * ratio; 
+        }       
     }
 }
