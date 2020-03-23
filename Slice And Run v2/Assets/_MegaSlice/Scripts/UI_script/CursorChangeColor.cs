145edb694fffe047c29dd039e1c0a4e445536458
Blot Paul <blot.paulluap@sfr.fr>
2020-03-23 10:40:51 +0100

    le curseur change de couleur si peut découper un truc

---
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/UI_script/CursorChangeColor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class CursorChangeColor : MonoBehaviour
+{
+	public Color colorTrigger = Color.red; 
+	public float sizeDetection = 10f;
+	Image img; 
+	Transform camTransform;
+
+
+	Color originalColor; 
+
+	private void Start()
+	{
+		camTransform = Camera.main.transform;
+		img = GetComponent<Image>();
+		originalColor = img.color; 
+	}
+
+	private void Update()
+	{
+		RaycastHit hit;
+		if (Physics.Raycast(camTransform.position, camTransform.TransformDirection(Vector3.forward), out hit, sizeDetection))
+		{
+			if (hit.collider.gameObject.tag == "Sliceable")
+			{
+				img.color = colorTrigger;
+			}
+			else
+			{
+				img.color = originalColor; 
+			}
+		}
+	}
+
+}
