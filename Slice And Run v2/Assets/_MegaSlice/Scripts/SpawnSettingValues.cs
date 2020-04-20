1c6a43575854705b475de03fda25802a080176e4
Rosam-dev <blot.paulluap@sfr.fr>
2020-04-20 15:54:42 +0200

    setting values (WIP)

---
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/SpawnSettingValues.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSettingValues : MonoBehaviour
+{
+    public GameObject settingValue; 
+    
+
+    void Start()
+    {
+        GameObject otherSetting = GameObject.FindGameObjectWithTag("SettingValues"); 
+        if (otherSetting == null)
+        {
+            Instantiate(settingValue, transform.position, transform.rotation); 
+        }
+    }
+
+
+}
