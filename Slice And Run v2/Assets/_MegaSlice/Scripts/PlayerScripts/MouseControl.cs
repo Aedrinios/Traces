a8abf1f5ba0a6f40e5a49a80a7be6d06c56ce5ce
Rosam-dev <blot.paulluap@sfr.fr>
2020-04-24 00:26:24 +0200

    control rotation (wip)

--- Slice And Run v2/Assets/_MegaSlice/Scripts/PlayerScripts/MouseControl.cs
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/PlayerScripts/MouseControl.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class MouseControl : MonoBehaviour
 {
-    [Header("Parameter rotation nul")]
-    public static float angle;
-    public static Vector3 inputCut { get; private set; }
-    public float mouseSensitivity;
+    public float maxSensivity = 2.5f;
+    public float minSensivity = 0.1f;
+    public float frameRefesh = 1;
+    //public float minAngleChange = 5; 
+
+    Vector3 inputMouse;
+    float chrono = 0; 
 
     [Header("Parameter rotation collot")]
     public List<Vector2> oldMousePositions = new List<Vector2>();
@@ -19,51 +22,14 @@ public class MouseControl : MonoBehaviour
     [SerializeField, Range(0, 1)] float smooth = 0.1f;
     public static float currentAngle;
     float refAngle;
-
     public bool collotRotation;
-
-    Vector2 MousePosition
-    {
-        get
-        {
-            return Input.mousePosition;
-            //float mouseX = Input.GetAxisRaw("Mouse X");
-            //float mouseY = Input.GetAxisRaw("Mouse Y");
-            //return new Vector2(mouseX, mouseY);
-        }
-    }
+    Vector2 MousePosition { get { return Input.mousePosition; } }
 
     void Update()
     {
         if (collotRotation)
         {
-            oldMousePositions.Add(MousePosition);
-            if (oldMousePositions.Count > maxPoints)
-                oldMousePositions.RemoveAt(0);
-
-            Vector2 mouseDelta;
-            int referencePointId = oldMousePositions.Count - 1;
-            while (Vector2.Distance(MousePosition, oldMousePositions[referencePointId]) < minDistance)
-            {
-                referencePointId--;
-                if (referencePointId < 0)
-                    break;
-            }
-
-            if (referencePointId < 0)
-            {
-                mouseDelta = oldMouseDelta;
-            }
-            else
-            {
-                mouseDelta = MousePosition - oldMousePositions[referencePointId];
-                oldMouseDelta = mouseDelta;
-            }
-
-            float targetAngle = -Vector2.SignedAngle(mouseDelta, Vector2.right);
-            targetAngle = Mathf.SmoothDamp(currentAngle, targetAngle, ref refAngle, smooth);
-            //  transform.localEulerAngles = new Vector3(0, 0, targetAngle);
-            currentAngle = targetAngle;
+            CollotCalculateRotation(); 
         }
         else
         {
@@ -73,19 +39,58 @@ public class MouseControl : MonoBehaviour
 
     void CalculateAxeMouse()
     {
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
-
-        Vector3 inputMouse = (new Vector3(mouseX, mouseY));
-        if (inputMouse.magnitude >= mouseSensitivity)
+        chrono += Time.deltaTime; 
+        if (chrono >= Time.deltaTime * frameRefesh)
         {
-            inputCut = inputMouse.normalized;
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+            inputMouse = new Vector3(mouseX, mouseY, 0);
+            chrono = 0;
         }
 
-        if (inputCut != Vector3.zero)
+        if (inputMouse.magnitude >= minSensivity && inputMouse.magnitude < maxSensivity)
         {
-            Vector2 cut_2D = new Vector2(inputCut.x, inputCut.y);
-            currentAngle = Vector2.SignedAngle(-Vector2.right, cut_2D);
+            float newAngle = -Vector2.SignedAngle(inputMouse, Vector2.right);
+            currentAngle = newAngle;
         }
+    } 
+
+
+    float InverseAngle(float angle)
+    {
+        Vector3 inverseVector = Quaternion.AngleAxis(angle, Vector3.forward) * -Vector3.right;
+        float inverse = -Vector2.SignedAngle(inverseVector, Vector2.right);
+        return inverse;
+    }
+
+    void CollotCalculateRotation()
+    {
+        oldMousePositions.Add(MousePosition);
+        if (oldMousePositions.Count > maxPoints)
+            oldMousePositions.RemoveAt(0);
+
+        Vector2 mouseDelta;
+        int referencePointId = oldMousePositions.Count - 1;
+        while (Vector2.Distance(MousePosition, oldMousePositions[referencePointId]) < minDistance)
+        {
+            referencePointId--;
+            if (referencePointId < 0)
+                break;
+        }
+
+        if (referencePointId < 0)
+        {
+            mouseDelta = oldMouseDelta;
+        }
+        else
+        {
+            mouseDelta = MousePosition - oldMousePositions[referencePointId];
+            oldMouseDelta = mouseDelta;
+        }
+
+        float targetAngle = -Vector2.SignedAngle(mouseDelta, Vector2.right);
+        targetAngle = Mathf.SmoothDamp(currentAngle, targetAngle, ref refAngle, smooth);
+        //  transform.localEulerAngles = new Vector3(0, 0, targetAngle);
+        currentAngle = targetAngle;
     }
 }
