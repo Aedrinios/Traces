945c7c548c032db8d3d221df7808afb670bbebd1
Rosam-dev <blot.paulluap@sfr.fr>
2020-04-28 15:13:07 +0200

    Ajout de la musique !!!
    (pensez à build fmod)

---
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/Utilities/SpawnOnePrefabs.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOnePrefabs : MonoBehaviour
+{
+    public GameObject prefabSpawn;
+    public string tagPrefab; 
+
+    void Awake()
+    {
+        SpawnOnlyOnePrefabs(); 
+    }
+
+    void SpawnOnlyOnePrefabs()
+    {
+        GameObject oldPrefab = GameObject.FindGameObjectWithTag(tagPrefab); 
+        if (oldPrefab == null)
+        {
+            Instantiate(prefabSpawn, transform.position, transform.rotation); 
+        }
+    }
+    
+}
