7579f53bf79a0ea91245b87b2c50c69bcc0baca4
Aedrinios <clementclerc2@gmail.com>
2020-04-29 23:01:45 +0200

    canon shoot when player in sight

--- Slice And Run v2/Assets/_MegaSlice/Scripts/CanonBehaviour.cs
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/CanonBehaviour.cs
@@ -10,23 +10,41 @@ public class CanonBehaviour : MonoBehaviour
     public Transform turret;
 
     private Transform target;
+    private int layerMask = 1 << 15;
+
 
     private bool isReloading = true;
     [SerializeField] private float reloadTimer;
     [SerializeField] private bool followPlayer;
+    public bool canShoot;
     public float offSetYTarget = 0.2f; 
     private float timer = 0f;
 
     private void Start()
     {
         target = GameObject.FindWithTag("Player").transform;
+        layerMask = ~layerMask;
         turret.LookAt(target.transform.position + Vector3.up * offSetYTarget);
     }
 
     void Update()
     {
+        Debug.DrawRay(muzzle.position, muzzle.forward + Vector3.up * -0.01f, Color.red);
         if (ChronoSystem.playing)
         {
+            RaycastHit hit;
+            Vector3 rayDirection = target.position - muzzle.position;
+            if (Physics.Raycast(muzzle.position, rayDirection, out hit, Mathf.Infinity, layerMask))
+            {
+                if (hit.collider.tag == "Player")
+                {
+                    canShoot = true;
+                }
+                else
+                {
+                    canShoot = false;
+                }
+            }
             if (followPlayer)
             {
                 turret.LookAt(target.transform.position + Vector3.up * offSetYTarget);
@@ -43,7 +61,10 @@ public class CanonBehaviour : MonoBehaviour
             }
             else
             {
-                Shoot();
+                if (canShoot)
+                {
+                    Shoot();
+                }
             }
         }
     }
@@ -51,7 +72,7 @@ public class CanonBehaviour : MonoBehaviour
     void Shoot()
     {
         GameObject bullet = Instantiate(bulletPrefab, muzzle.position, Quaternion.LookRotation(turret.forward));
-        BulletBehaviour bulletBehaviour = bullet.GetComponent<BulletBehaviour>();
+        BulletBehaviour bulletBehaviour = bullet.transform.GetChild(0).GetComponent<BulletBehaviour>();
         bulletBehaviour.direction = muzzle.forward;
         isReloading = true;
     }
