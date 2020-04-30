7bdc2c094304217c503ec26d7ce44085d0883342
Taramelg <38104144+Taramelg@users.noreply.github.com>
2020-04-30 17:00:54 +0200

    Push ingrédients wipeout et SprintWall
    :cold_sweat:

---
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/AnimScripts/sprintwall_Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sprintwall_Controller : MonoBehaviour
+{
+    public Animator sp_Wall;
+    public BoxCollider boite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sp_Wall = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            sp_Wall.Play("SprintWall");
+            Destroy(boite);
+        }
+    }
+}
---
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/AnimScripts/wipeoutController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wipeoutController : MonoBehaviour
+{
+    public Animator wipe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wipe = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            wipe.Play("Take 001");
+        }
+    }
+}
