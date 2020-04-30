bbd3ab66eda640d86c6cac3b4223bc71db8cb1a7
Rosam-dev <blot.paulluap@sfr.fr>
2020-04-30 11:06:41 +0200

    Musique adaptative

--- Slice And Run v2/Assets/_MegaSlice/Scripts/SoundScripts/ChangeMusicParameter.cs
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/SoundScripts/ChangeMusicParameter.cs
@@ -10,12 +10,24 @@ public class ChangeMusicParameter : MonoBehaviour
     [FMODUnity.EventRef]
     public string fmodEvent;
     public float transitionStartSpeed = 0.1f;
-    public float transitionEndSpeed = 0.1f; 
-    float valueState = 0;
+    public float transitionEndSpeed = 0.1f;
+    public float delayClamTheme = 12f; 
 
+    float valueState = 0;
     bool isRun = true;
 
-    bool playerStart = false; 
+    Vector3 oldPostionPlayer;
+    float chrono = 0;
+    public int sceneIndex; 
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        instance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
+        instance.start();
+
+        valueState = 0;      
+    }
 
     private void Update()
     {
@@ -29,50 +41,43 @@ public class ChangeMusicParameter : MonoBehaviour
         {
             valueState += transitionEndSpeed * Time.deltaTime;
         }
-
-        if (!playerStart)
-        {
-            if (Input.anyKeyDown)
-            {
-                playerStart = true;
-                isRun = true; 
-            }
-        }
-    }
-
-    void OnEnable()
-    {
-        instance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
-        instance.start();
-
-        SceneManager.sceneLoaded += OnSceneLoaded;
-
-        LevelManager.onLevelComplete += BoardParameter;
-        LevelManager.onLevelFailed += BoardParameter;        
+        valueState = Mathf.Clamp(valueState, 0f, 1f); 
+        DetectActionPlayer(); 
     }
 
     private void OnDisable()
     {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
         instance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+    }
 
-        LevelManager.onLevelComplete -= BoardParameter;
-        LevelManager.onLevelFailed -= BoardParameter;
+    void DetectActionPlayer()
+    {
+        if (chrono >= delayClamTheme)
+        {
+            isRun = false;
+        }
+
+        if (Vector3.Distance(oldPostionPlayer, FPS_Controller.playerPos) <= 0.1f)
+        {
+            chrono += Time.deltaTime; 
+        }
+        else
+        {
+            isRun = true;
+            chrono = 0; 
+        }
+        oldPostionPlayer = FPS_Controller.playerPos; 
     }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        playerStart = false; 
-    }
+        sceneIndex = scene.buildIndex;
 
-    void RunParameter()
-    {
-        isRun = true;
+        if (sceneIndex == 0)
+        {
+            Destroy(gameObject); 
+        }
     }
-
-    void BoardParameter()
-    {
-        isRun = false;
-    }
-
 
 }
