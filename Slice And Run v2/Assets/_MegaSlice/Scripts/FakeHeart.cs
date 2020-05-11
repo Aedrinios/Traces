6080e693f7b0ae5f047a0ef9d529fc3f97bcb9cd
Rosam-dev <blot.paulluap@sfr.fr>
2020-05-11 16:11:03 +0200

    Door + FakeHeart

---
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/FakeHeart.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FakeHeart : MonoBehaviour
+{
+    public GameObject doorFinish;
+    public GameObject particleEndPrefab;
+    public GameObject soundSliceEnd;
+    public float scaleDistance = 1;
+    private Transform parent;
+
+    SliceableObject sliceScript;
+    private float originalTimeScale;
+    private float timePast;
+    private bool slowed;
+
+    private void Start()
+    {
+        parent = transform.parent;
+        sliceScript = GetComponent<SliceableObject>();
+        originalTimeScale = Time.timeScale;
+        timePast = 0f;
+        slowed = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Projectile") && sliceScript.canSlice)
+        {
+            EndGame(other.gameObject.transform);
+        }
+    }
+
+    void EndGame(Transform projectile)
+    {
+        GameObject newEffect = Instantiate(particleEndPrefab, parent.position, projectile.rotation) as GameObject;
+        float distance = Vector3.Distance(transform.position, FPS_Controller.playerPos);
+
+
+        Transform[] childs = newEffect.transform.GetComponentsInChildren<Transform>();
+        for (int i = 0; i < childs.Length; i++)
+        {
+            childs[i].localScale *= Mathf.Sqrt((distance + 1)) * scaleDistance;
+        }
+
+        doorFinish.GetComponent<FinishDoorManager>().trigger = true; 
+
+        GameObject newSound = Instantiate(soundSliceEnd, transform.position, transform.rotation);
+
+    }
+}
---
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/LevelDesignScripts/FinishDoorManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishDoorManager : MonoBehaviour
+{
+    public float delayBeforeStart = 1f; 
+    public float delay = 0.25f;
+    public bool trigger = false;
+
+    private void Update()
+    {
+        if (trigger)
+        {
+            Invoke("StartOpenDoor", delayBeforeStart); 
+            trigger = false; 
+        }
+    }
+
+    public void StartOpenDoor()
+    {
+        StartCoroutine(OpenDoor());
+    }
+
+    IEnumerator OpenDoor()
+    {
+        bool playing = true;
+        float chrono = 0;
+
+        WipeoutSystem[] wipeoutScript = GetComponentsInChildren<WipeoutSystem>(); 
+        int i = 0; 
+
+        while (playing)
+        {
+            chrono += Time.deltaTime; 
+            if (chrono >= delay)
+            {
+                wipeoutScript[i].trigger = true;
+                i++; 
+                chrono = 0; 
+            }
+            if (i >= wipeoutScript.Length)
+            {
+                playing = false; 
+            }
+            yield return null; 
+        }
+    }
+
+}
