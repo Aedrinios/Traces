caea623cb420a16644827945a606e05a05182f07
Rosam-dev <blot.paulluap@sfr.fr>
2020-06-08 12:33:23 +0200

    trop simple

---
+++ Slice And Run v2/Assets/_MegaSlice/Scripts/DetectLanguageSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectLanguageSystem : MonoBehaviour
+{
+    void Start()
+    {
+        //This checks if your computer's operating system is in the French language
+        if (Application.systemLanguage == SystemLanguage.French)
+        {
+            //Outputs into console that the system is French
+            Debug.Log("This system is in French. ");
+        }
+        //Otherwise, if the system is English, output the message in the console
+        else 
+        {
+            Debug.Log("This system is in English. ");
+        }
+    }
+}
